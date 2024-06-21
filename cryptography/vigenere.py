3403bbf0b1bb1d80058212073f621aa8778d5ebf
Ltk-Mxz <a96.paul96@gmail.com>
2024-06-21 13:37:48 +0000

    first commit

---
+++ cryptography/vigenere.py
@@ -0,0 +1,28 @@
+text = 'How are you bro ?'
+custom_key = 'IlovePython'
+
+def vigenere(message, key, direction=1):
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    final_message = ''
+
+    for char in message.lower():
+
+        if not char.isalpha():
+            final_message += char
+        else:        
+            key_char = key[key_index % len(key)]
+            key_index += 1
+
+            offset = alphabet.index(key_char)
+            index = alphabet.find(char)
+            new_index = (index + offset*direction) % len(alphabet)
+            final_message += alphabet[new_index]
+    
+    return final_message
+
+def encrypt(message, key):
+    return vigenere(message, key)
+    
+def decrypt(message, key):
+    return vigenere(message, key, -1)
