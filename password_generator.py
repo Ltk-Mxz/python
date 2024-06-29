ff5b2fb8bea39be7e49cb241d777f6105d934e6c
Ltk-Mxz <a96.paul96@gmail.com>
2024-06-29 19:40:59 +0000

    Add password_generator.py to the repository !

---
+++ password_generator.py
@@ -0,0 +1,34 @@
+import re
+import secrets
+import string
+
+
+def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase=1):
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+
+    all_characters = letters + digits + symbols
+
+    while True:
+        password = ''
+        for _ in range(length):
+            password += secrets.choice(all_characters)
+        
+        constraints = [
+            (nums, r'\d'),
+            (special_chars, fr'[{symbols}]'),
+            (uppercase, r'[A-Z]'),
+            (lowercase, r'[a-z]')
+        ]
+
+        if all(
+            constraint <= len(re.findall(pattern, password))
+            for constraint, pattern in constraints
+        ):
+            break
+    
+    return password
+    
+new_password = generate_password()
+print('Generated password:', new_password)
